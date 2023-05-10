944bedb097a91964310e8084db854289f8244e7f
Ninf0 <118858421+Ninf0@users.noreply.github.com>
2023-05-10 03:56:25 -0300

    avance

--- Assets/3_Scrip/Game/Torreta/Turret_GRAL.cs
+++ Assets/3_Scrip/Game/Torreta/Turret_GRAL.cs
@@ -2,69 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 
 public class Turret_GRAL : MonoBehaviour
 {
-    public GameObject arrowPrefab;
-    public float radius = 10f;
-    public float fireDelay = 5f;
-    private Transform target;
-    
-    private void Update()
+    public float buyPrice;
+    public float sellPrice;
+
+
+    public LayerMask layerDetection;
+    public float range;
+    public Zombie currentTarget;
+    public List<Zombie> currentTargets = new List<Zombie>();
+
+    private void EnemyDetection()
     {
-       /* // Si hay un objetivo actual, lo seguimos con la torreta
-        if (target != null)
-        {
-            // Rotamos la torreta hacia el objetivo
-            Vector3 direction = target.position - transform.position;
-            Quaternion rotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10f);
+        var enemy = Physics.OverlapSphere(transform.position, range).where(currentEnemy.Getcomponent<Zombie>())
 
-            // Si el objetivo está fuera del radio, lo perdemos
-            if (direction.magnitude > radius)
-            {
-                target = null;
-            }
-        }
-        // Si no hay un objetivo actual, buscamos uno dentro del radio
-        else
-        {
-            Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
-            float closestDistance = Mathf.Infinity;
-
-            foreach (Collider collider in colliders)
-            {
-                // Si el collider tiene el tag "Enemy", lo consideramos como objetivo
-                if (collider.CompareTag("Enemy"))
-                {
-                    // Calculamos la distancia entre la torreta y el enemigo
-                    float distance = (collider.transform.position - transform.position).magnitude;
-
-                    // Si el enemigo está dentro del radio y es el más cercano lo seleccionamos como objetivo
-                    if (distance < radius && distance < closestDistance)
-                    {
-                        closestDistance = distance;
-                        target = collider.transform;
-                    }
-                }
-            }
-        }*/
-
-        //  tiempo de espera, disparamos 
-        if (target != null && Time.time > fireDelay)
-        {
-            // Creamos una instancia de la flecha en la posición de la torreta
-            GameObject arrowInstance = Instantiate(arrowPrefab, transform.position, transform.rotation);
-
-            // Hacemos que la flecha mire hacia el objetivo
-            Vector3 direction = target.position - arrowInstance.transform.position;
-            Quaternion rotation = Quaternion.LookRotation(direction);
-            arrowInstance.transform.rotation = rotation;
-
-            // Reseteamos el tiempo de espera
-            fireDelay = Time.time + 5f;
-        }
     }
+
+
 }
+
 
