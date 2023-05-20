7faad16d9771fc1915e071e1fd298776ec12914b
Ninf0 <118858421+Ninf0@users.noreply.github.com>
2023-05-19 23:01:57 -0300

    .

--- Assets/_Scripts/ScriptableObjectsScripts/TurretData.cs
+++ Assets/_Scripts/ScriptableObjectsScripts/TurretData.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TurretData", menuName = "ScriptableObjects/TurretData", order = 1)]
 public class TurretData : ScriptableObject
 {
-  [SerializeField] private Turret_GRAL _turretPrefab;
+ /* [SerializeField] private Turret_GRAL _turretPrefab;
   [SerializeField] private int _costAmount;
   [SerializeField] private int _upgradeCostAmount;
   [SerializeField] private int _upgradeCount;
 
   public int SellAmount => _costAmount - (_costAmount / 4);
   public Turret_GRAL TurretPrefab => _turretPrefab;
-  public int CostAmount => _costAmount;
+  public int CostAmount => _costAmount; */
+
+
+
+    [SerializeField] private Turret_GRAL _turretPrefab;
+    [SerializeField] private int _costAmount;
+    [SerializeField] private int _upgradeCostAmount;
+    [SerializeField] private int _upgradeCount;
+
+    public int SellAmount => _costAmount - (_costAmount / 4);
+    public Turret_GRAL TurretPrefab => _turretPrefab;
+    public int CostAmount => _costAmount;
 
 }
+
