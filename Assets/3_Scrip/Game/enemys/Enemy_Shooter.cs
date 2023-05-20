ac14ea88d91570414fe41bb92d59adc6f541ebfd
Ninf0 <118858421+Ninf0@users.noreply.github.com>
2023-05-19 22:41:41 -0300

    .

--- Assets/3_Scrip/Game/enemys/Enemy_Shooter.cs
+++ Assets/3_Scrip/Game/enemys/Enemy_Shooter.cs
@@ -4,54 +4,49 @@ using UnityEngine;
 using UnityEngine.AI;
 public class Enemy_Shooter : LifeEntities
 {
-   
-        public Transform playerPosition;
-        public GameObject axePrefab;
-        public Transform axeSpawnPoint;
-        public float shootInterval = 3f;
-        public float speedProj = 10f;
-        public Animator anim;
+    public Transform playerPosition;
+    public GameObject axePrefab;
+    public Transform axeSpawnPoint;
+    public float shootInterval = 3f;
+    public float speedProj = 10f;
 
-        private NavMeshAgent navMeshAgent;
-        private float shootTimer = 0f;
+    private NavMeshAgent navMeshAgent;
+    private float shootTimer = 0f;
 
-        private const float shootingDistance = 1.5f;
+    private const float shootingDistance = 1.5f;
 
-        private bool isWalking = false;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        InvokeRepeating("ShootPlayer", shootInterval, shootInterval);
+    }
 
-        void Start()
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, playerPosition.position);
+
+        if (distanceToPlayer <= shootingDistance)
         {
-            navMeshAgent = GetComponent<NavMeshAgent>();
-            anim = GetComponent<Animator>();
-            InvokeRepeating("ShootPlayer", shootInterval, shootInterval);
+            // Disparar al jugador
+            anim.SetTrigger("ZOMBIE_ATTACK");
+            navMeshAgent.isStopped = true;
         }
-
-        void Update()
+        else
         {
-            float distanceToPlayer = Vector3.Distance(transform.position, playerPosition.position);
-
-            if (distanceToPlayer <= shootingDistance)
-            {
-                // Disparar al jugador
-                anim.SetTrigger("ZOMBIE_ATTACK");
-                navMeshAgent.isStopped = true;
-            }
-            else
-            {
-                // Caminar hacia el jugador
-                navMeshAgent.isStopped = false;
-                navMeshAgent.SetDestination(playerPosition.position);
-                anim.SetTrigger("Z_WALK");
-            }
-        }
-
-        void ShootPlayer()
-        {
-            Vector3 directionToPlayer = (playerPosition.position - axeSpawnPoint.position).normalized;
-
-            GameObject newAxe = Instantiate(axePrefab, axeSpawnPoint.position, axeSpawnPoint.rotation);
-            Rigidbody axeRigidbody = newAxe.GetComponent<Rigidbody>();
-            axeRigidbody.velocity = axeSpawnPoint.forward * speedProj;
+            // Caminar hacia el jugador
+            navMeshAgent.isStopped = false;
+            navMeshAgent.SetDestination(playerPosition.position);
+            anim.SetTrigger("Z_WALK");
         }
     }
+
+    void ShootPlayer()
+    {
+        Vector3 directionToPlayer = (playerPosition.position - axeSpawnPoint.position).normalized;
+
+        GameObject newAxe = Instantiate(axePrefab, axeSpawnPoint.position, axeSpawnPoint.rotation);
+        Rigidbody axeRigidbody = newAxe.GetComponent<Rigidbody>();
+        axeRigidbody.velocity = axeSpawnPoint.forward * speedProj;
+    }
+}
 
